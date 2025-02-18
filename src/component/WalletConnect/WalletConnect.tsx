b43a79aced1df8ec4094727604019d346b863361
Asfandope <asfand798@gmail.com>
2025-02-18 16:58:38 +0500

    Delete src directory

--- src/component/WalletConnect/WalletConnect.tsx
+++
@@ -1,5 +0,0 @@
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import "./index.css"
-export default () => {
-  return <WalletMultiButton />;
-};
--- src/config/wallet.config.ts
+++
@@ -1,4 +0,0 @@
-export const walletConfig = {
-    externalConnectionUrl: 'https://lightslategray-armadillo-425266.hostingersite.com/phantom-connect.html',
-    apiEndpoint: '/api/wallet/external-connect',
-}; 
